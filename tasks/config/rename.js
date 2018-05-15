076d2e2c6113734271f45d673e0dceeb0fc738f3
Ashwin <ayardi@grepruby.com>
2018-05-15 15:43:22 +0530

    Production JS and CSS Timestamp versioning removed

--- tasks/config/rename.js
+++ tasks/config/rename.js
@@ -4,11 +4,11 @@ module.exports = function(grunt) {
 	grunt.config.set('rename', {
 		js: {
 				src: '.tmp/public/min/production.min.js',
-            	dest: '.tmp/public/min/production.v'+timestamp+'.min.js'
+            	dest: '.tmp/public/min/production.min.js'
 			},
 		css: {
 				src: '.tmp/public/min/production.min.css',
-            	dest: '.tmp/public/min/production.v'+timestamp+'.min.css'
+            	dest: '.tmp/public/min/production.min.css'
 			}
 	});
 	grunt.loadNpmTasks('grunt-rename');
